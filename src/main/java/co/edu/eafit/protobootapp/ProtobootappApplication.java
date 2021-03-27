c174b84934b662628448e5127d9c96e87b121f33
Luisa Maria Vasquez Gomez <lmvasquezg@MacBook-Pro-de-Luisa.local>
2021-03-26 20:18:29 -0500

    Trigger pipeline

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -21,7 +21,7 @@ public class ProtobootappApplication {
     //Se ejecuta con http://localhost:8080/
     @GetMapping("/")
     public String index() {
-        return String.format("Bienvenido! Consulte el modo de uso de esta aplicación con el administrador. Aquí creamos cuadrados y cubos felices wii :)");
+        return String.format("Bienvenido! Aquí creamos cuadrados y cubos felices wii :)");
     }    
 
 //Se ejecuta con http://localhost:8080/hola o http://localhost:8080/hola?nombre=John
